3980017f6e69d3aaed0a938b896aa2797c42fff7
“se1ryu <mizer.piotr@gmail.com>
2018-04-08 13:45:03 +0200

    Dodano trudniejsza wersje

--- RockPaperScissorsGame/src/main/java/Computer.java
+++ RockPaperScissorsGame/src/main/java/Computer.java
@@ -1,11 +1,62 @@
 import java.util.*;
 
 public class Computer {
-    public Move getMove(){
+    public Move getMove() {
         Move[] moves = Move.values();
         Random random = new Random();
         int index = random.nextInt(moves.length);
-        return  moves[index];
+        return moves[index];
     }
 
+    public Move getMoveHard(Move playerMove) {
+            int win = (Math.random() <= 0.5) ? 0 : 1;
+            int temp = (Math.random() <= 0.5) ? 0 : 1;
+            int temp1 = (Math.random() <= 0.5) ? 0 : 1;
+
+            switch (playerMove) {
+                case ROCK:
+                    if(win==1) { // if computer wins
+                        return (temp == 1) ? Move.PAPER : Move.SPOCK;
+                    }
+                    if(temp==1){ // if computer losses
+                        return (temp1 == 1) ? Move.SCISSORS : Move.LIZARD;
+                    }
+                    return Move.ROCK;
+                case PAPER:
+                    if(win==1) {
+                        return (temp == 1) ? Move.SCISSORS : Move.LIZARD;
+                    }
+                    if(temp==1){
+                        return (temp1 == 1) ? Move.SPOCK : Move.ROCK;
+                    }
+                    return Move.PAPER;
+                case SCISSORS:
+                    if(win==1) {
+                        return (temp == 1) ? Move.ROCK : Move.SPOCK;
+                    }
+                    if(temp==1){
+                        return (temp1 == 1) ? Move.PAPER : Move.LIZARD;
+                    }
+                    return Move.SCISSORS;
+                case LIZARD:
+                    if(win==1) {
+                        return (temp == 1) ? Move.ROCK : Move.SCISSORS;
+                    }
+                    if(temp==1){
+                        return (temp1 == 1) ? Move.SPOCK : Move.PAPER;
+                    }
+                    return Move.LIZARD;
+                case SPOCK:
+                    if(win==1) {
+                        return (temp == 1) ? Move.PAPER : Move.LIZARD;
+                    }
+                    if(temp==1){
+                        return (temp1 == 1) ? Move.SCISSORS : Move.ROCK;
+                    }
+                    return Move.SPOCK;
+            }
+
+
+        return getMoveHard(playerMove);
+    }
 }
--- RockPaperScissorsGame/src/main/java/GameRunner.java
+++ RockPaperScissorsGame/src/main/java/GameRunner.java
@@ -8,6 +8,8 @@ public class GameRunner {
     private int computerScore;
     private int numberOfWins;
     private int totalRounds;
+    private int gameMode;
+    private static int gameOn;
 
 
     public GameRunner(){
@@ -16,7 +18,28 @@ public class GameRunner {
         userScore = 0;
         computerScore = 0;
         numberOfWins = 0;
+        gameMode = 0;
+        gameOn = 0;
 
+
+    }
+
+    public static void GameOff() {
+       gameOn = 1 ;
+    }
+
+    public int difficultySetting(){
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Czy chcialbys sprobowac nieuczciwy tryb? Komputer ma 50% szansy na wygrana.\n0 - Nie\n1 - Tak");
+        while (!inputScanner.hasNextInt()) {
+            System.out.println("To nie jest liczba.");
+            inputScanner.next();
+        }
+        gameMode = inputScanner.nextInt();
+        if(gameMode != 0 && gameMode != 1) {
+            return difficultySetting();
+        }
+        return gameMode;
 
     }
 
@@ -25,59 +48,72 @@ public class GameRunner {
         int losses = computerScore;
         int ties = totalRounds - userScore - computerScore;
 
-        System.out.println("Wygrales "+ wins + ", razy ");
-        System.out.println("Przegrales "+ losses + ", razy ");
-        System.out.println("Zremisowales "+ ties + ", razy ");
+        double winPercentage = (double)wins / totalRounds*100;
+        double lossesPercentage = (double)losses / totalRounds*100;
+        double tiesPercentage = (double)ties  / totalRounds*100;
+
+
+        System.out.println("Zagrana ilosc rund: " + totalRounds);
+        System.out.println("Wygrales "+ wins + ", razy " + String.format("%.2f", winPercentage)+"% wygranych");
+        System.out.println("Przegrales "+ losses + ", razy "+ String.format("%.2f", lossesPercentage)+"% przegranych");
+        System.out.println("Zremisowales "+ ties + ", razy "+ String.format("%.2f", tiesPercentage)+"% remisow");
+
     }
 
     public void gameRunner() {
+        while(gameOn == 0) {
+            System.out.println("Witam w grze Papier, Kamien, Nozyce, Spock!");
 
-        System.out.println("Do ilu wygranych rund chcialbys zagrac?");
-        Scanner inputScanner = new Scanner(System.in);
-        do {
-            System.out.println("Pamietaj, ze liczba wygranych musi byc dodatnia oraz calkowita.");
-            while (!inputScanner.hasNextInt()) {
-                System.out.println("To nie jest liczba.");
-                inputScanner.next();
+            Scanner inputScanner = new Scanner(System.in);
+
+            difficultySetting();
+
+            System.out.println("Do ilu wygranych rund chcialbys zagrac?");
+
+            do {
+                System.out.println("Pamietaj, ze liczba wygranych musi byc dodatnia oraz calkowita.");
+                while (!inputScanner.hasNextInt()) {
+                    System.out.println("To nie jest liczba.");
+                    inputScanner.next();
+                }
+                numberOfWins = inputScanner.nextInt();
             }
-            numberOfWins = inputScanner.nextInt();
-        }
-        while (numberOfWins <= 0);
+            while (numberOfWins <= 0);
 
 
-        while(userScore < numberOfWins && computerScore < numberOfWins) {
-            System.out.println("Zadeklaruj swoj wybor: " +
-                    "\n 1 - Papier " +
-                    "\n 2 - Kamien " +
-                    "\n 3 - Nozyce " +
-                    "\n 4 - Jaszczurka " +
-                    "\n 5 - Spock");
+            while (userScore < numberOfWins && computerScore < numberOfWins) {
+                System.out.println("Zadeklaruj swoj wybor: " +
+                        "\n 1 - Papier " +
+                        "\n 2 - Kamien " +
+                        "\n 3 - Nozyce " +
+                        "\n 4 - Jaszczurka " +
+                        "\n 5 - Spock");
 
 
-            Move userMove = user.getMove();
-            Move computerMove = computer.getMove();
-            System.out.println("Zagrales: " + userMove);
-            System.out.println("Komputer wybral: " + computerMove);
+                Move userMove = user.getMove();
+                Move computerMove = gameMode == 0 ? computer.getMove() : computer.getMoveHard(userMove);
+                System.out.println("Zagrales: " + userMove);
+                System.out.println("Komputer wybral: " + computerMove);
 
-            int compareMoves = userMove.compareMoves(computerMove);
-            switch (compareMoves) {
-                case 0:
-                    System.out.println("Remis.");
-                    break;
-                case 1:
-                    System.out.println(userMove + " pokonuje " + computerMove + ", wygrana!");
-                    userScore++;
-                    break;
-                case -1:
-                    System.out.println(userMove + " przegrywa z " + computerMove + ", przegrana");
-                    computerScore++;
-                    break;
+                int compareMoves = userMove.compareMoves(computerMove);
+                switch (compareMoves) {
+                    case 0:
+                        System.out.println("Remis.");
+                        break;
+                    case 1:
+                        System.out.println(userMove + " pokonuje " + computerMove + ", wygrana!");
+                        userScore++;
+                        break;
+                    case -1:
+                        System.out.println(userMove + " przegrywa z " + computerMove + ", przegrana");
+                        computerScore++;
+                        break;
+                }
+                totalRounds++;
             }
-            totalRounds++;
+            printGameStats();
+
+            user.playAgain();
         }
-        printGameStats();
-
-        user.playAgain();
-
     }
 }
